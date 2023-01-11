9b5f6a95bdba581eb69a1e2d2b36bb76fba2b5e3
jhyunwoo <jhyunwoo0228@gmail.com>
2023-01-11 23:54:18 +0900

    add loading ui

---
+++ components/Loading.js
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="bg-slate-50 flex justify-center items-center w-full h-screen">
+      <div className="text-xl font-serif">Loading...</div>
+    </div>
+  )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,40 @@
 import "../styles/globals.css"
 import { SessionProvider } from "next-auth/react"
-import { useRouter } from "next/router"
+import { useEffect, useState } from "react"
+import Router from "next/router"
+
+import Loading from "../components/Loading"
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }) {
-  return (
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    const start = () => {
+      // NProgress.start();
+      setLoading(true)
+    }
+    const end = () => {
+      // NProgress.done();
+      setLoading(false)
+    }
+
+    Router.events.on("routeChangeStart", start)
+    Router.events.on("routeChangeComplete", end)
+    Router.events.on("routeChangeError", end)
+
+    return () => {
+      Router.events.off("routeChangeStart", start)
+      Router.events.off("routeChangeComplete", end)
+      Router.events.off("routeChangeError", end)
+    }
+  }, [])
+
+  return loading ? (
+    <Loading />
+  ) : (
     <SessionProvider session={session}>
       <Component {...pageProps} />
     </SessionProvider>
