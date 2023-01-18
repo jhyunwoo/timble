f5dcf4b10abcca7422d0e725db5437b36359f7ab
jhyunwoo <jhyunwoo0228@gmail.com>
2023-01-18 11:15:33 +0900

    add vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import { SessionProvider } from "next-auth/react"
 import { RecoilRoot } from "recoil"
 import React from "react"
 import Loading from "../components/Loading"
+import { Analytics } from "@vercel/analytics/react"
 
 export default function App({
   Component,
@@ -13,6 +14,7 @@ export default function App({
       <SessionProvider session={session}>
         <React.Suspense fallback={<Loading />}>
           <Component {...pageProps} />
+          <Analytics />
         </React.Suspense>
       </SessionProvider>
     </RecoilRoot>
