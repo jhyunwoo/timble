553ced24e8d7a4d9dca7439b017e54e64cca81e8
jhyunwoo <jhyunwoo0228@gmail.com>
2023-01-19 01:06:27 +0900

    add loading animation

--- components/Loading.js
+++ components/Loading.js
@@ -1,7 +1,9 @@
+import { WaveTopBottomLoading } from "react-loadingg"
+
 export default function Loading() {
   return (
     <div className="bg-slate-50 flex justify-center items-center w-full h-screen">
-      <div className="text-2xl font-semibold">Loading...</div>
+      <WaveTopBottomLoading color="#22c55e" />
     </div>
   )
 }
