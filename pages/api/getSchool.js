6054f4daf4fb28b7474cd5b37b8edc6095ab4086
jhyunwoo <jhyunwoo0228@gmail.com>
2023-01-22 16:09:35 +0900

    try to fix getschool function 8

--- pages/api/getSchool.js
+++ pages/api/getSchool.js
@@ -1,42 +1,28 @@
-import prisma from '../../lib/prismadb'
-import { authOptions } from './auth/[...nextauth]'
-import { unstable_getServerSession } from 'next-auth/next'
+import prisma from "../../lib/prismadb"
+import { authOptions } from "./auth/[...nextauth]"
+import { unstable_getServerSession } from "next-auth/next"
 
 export default async function getSchool(req, res) {
   const { schoolId } = req.body
   const session = await unstable_getServerSession(req, res, authOptions)
-  console.log(schoolId)
+
   if (session) {
-    try {
-      const schoolInfo = await prisma.school.findUnique({
-        where: {
-          id: schoolId,
-        },
-        include: {
-          subjects: {
-            id: true,
-            title: true,
-            diplomaId: true,
-            open: true,
-            type: true,
-            target: true,
-            relatedMajor: true,
-          },
-          diplomas: {
-            id: true,
-            name: true,
-            description: true,
-          },
-        },
-      })
-      console.log(schoolInfo)
-      res.json(schoolInfo)
-    } catch {
-      res.status(501)
-    }
+    const schoolInfo = await prisma.school.findUnique({
+      where: {
+        id: schoolId,
+      },
+      select: {
+        id: true,
+        code: true,
+        name: true,
+        color: true,
+        diplomas: true,
+      },
+    })
+    res.json(schoolInfo)
   } else {
     res.status(401).json({
-      message: 'You must be sign in to view the protected content on this page.',
+      message: "You must be sign in to view the protected content on this page.",
     })
   }
   res.end()
