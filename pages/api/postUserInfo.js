7563020c6f875c9c42c7854d3c35f2fc0ec719e9
jhyunwoo <jhyunwoo0228@gmail.com>
2023-01-22 15:35:11 +0900

    try to fix getschool function

--- pages/api/postUserInfo.js
+++ pages/api/postUserInfo.js
@@ -1,27 +1,31 @@
-import prisma from "../../lib/prismadb"
-import { authOptions } from "../api/auth/[...nextauth]"
-import { unstable_getServerSession } from "next-auth/next"
+import prisma from '../../lib/prismadb'
+import { authOptions } from '../api/auth/[...nextauth]'
+import { unstable_getServerSession } from 'next-auth/next'
 
 export default async function getUserNullData(req, res) {
   const session = await unstable_getServerSession(req, res, authOptions)
 
   if (session) {
     const { post, userEmail, data } = req.body
-    if (post === "schoolId") {
-      const postSchool = await prisma.school.update({
-        where: {
-          code: data,
-        },
-        data: {
-          students: {
-            connect: {
-              email: userEmail,
+    if (post === 'schoolId') {
+      try {
+        const postSchool = await prisma.school.update({
+          where: {
+            code: data,
+          },
+          data: {
+            students: {
+              connect: {
+                email: userEmail,
+              },
             },
           },
-        },
-      })
+        })
+      } catch {
+        const postSchool = 'no data'
+      }
       res.status(200)
-    } else if (post === "diploma") {
+    } else if (post === 'diploma') {
       const postDiploma = await prisma.diploma.update({
         where: {
           id: data,
@@ -35,7 +39,7 @@ export default async function getUserNullData(req, res) {
         },
       })
       res.status(200)
-    } else if (post === "year") {
+    } else if (post === 'year') {
       const postYear = await prisma.user.update({
         where: {
           email: userEmail,
@@ -45,7 +49,7 @@ export default async function getUserNullData(req, res) {
         },
       })
       res.status(200)
-    } else if (post === "name") {
+    } else if (post === 'name') {
       const postName = await prisma.user.update({
         where: {
           email: userEmail,
@@ -58,8 +62,7 @@ export default async function getUserNullData(req, res) {
     }
   } else {
     res.status(403).json({
-      message:
-        "You must be sign in to view the protected content on this page.",
+      message: 'You must be sign in to view the protected content on this page.',
     })
   }
   res.end()
