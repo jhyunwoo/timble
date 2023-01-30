13f15da37095e89a1a80860085dacef52309418a
jhyunwoo <jhyunwoo0228@gmail.com>
2023-01-30 14:02:33 +0900

    edge function test 1

--- pages/api/schools/subjects/[code]/index.js
+++ pages/api/schools/subjects/[code]/index.js
@@ -4,6 +4,10 @@ import { unstable_getServerSession } from "next-auth/next"
 
 export const config = {
   runtime: "edge",
+  unstable_allowDynamic: [
+    "/lib/utilities.js", // allows a single file
+    "/node_modules/function-bind/**", // use a glob to allow anything in the function-bind 3rd party module
+  ],
 }
 
 export default async function handler(req, res) {
