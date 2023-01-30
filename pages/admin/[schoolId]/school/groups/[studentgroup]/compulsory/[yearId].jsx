ab9bb9cfec0c58433e6b60841527afac0fa1c894
전현우 <jhyunwoo@Moveto.local>
2023-01-30 13:52:17 +0900

    add subject on compulsory

--- pages/admin/[schoolId]/school/groups/[studentgroup]/compulsory/[yearId].jsx
+++ pages/admin/[schoolId]/school/groups/[studentgroup]/compulsory/[yearId].jsx
@@ -1,9 +1,12 @@
 import { useRouter } from "next/router"
+import { useEffect, useState } from "react"
 import ProtectedPage from "../../../../../../../components/ProtectedPage"
 import useGroupSubjects from "../../../../../../../lib/client/useGroupSubjects"
+import MoveBack from "../../../../../../../components/MoveBack"
 
 export default function ConfigCoupulsory() {
   const router = useRouter()
+  const [filteredSubject, setFilteredSubject] = useState()
   const { groupSubjects } = useGroupSubjects(groupId())
   function yearId() {
     return router.query.yearId
@@ -11,14 +14,43 @@ export default function ConfigCoupulsory() {
   function groupId() {
     return router.query.studentgroup
   }
-  console.log(groupSubjects)
+  function schoolId() {
+    return router.query.schoolId
+  }
+
+  function filter() {
+    let subjects = []
+    if (groupSubjects) {
+      groupSubjects.map((data) => {
+        if (yearId() === "1") {
+          if (data.open.includes("1-1") || data.open.includes("1-2")) {
+            subjects.push(data)
+          }
+        } else if (yearId() === "2") {
+          if (data.open.includes("2-1") || data.open.includes("2-2")) {
+            subjects.push(data)
+          }
+        } else if (yearId() === "3") {
+          if (data.open.includes("3-1") || data.open.includes("3-2")) {
+            subjects.push(data)
+          }
+        }
+      })
+    }
+    setFilteredSubject(subjects)
+  }
+  useEffect(() => {
+    filter()
+  }, [groupSubjects])
+
   return (
     <ProtectedPage>
+      <MoveBack title="필수 이수 과목 설정" link={`/admin/${schoolId()}/school/groups/${groupId()}/compulsory`} />
       <div className="p-4">
         <div className="text-2xl font-bold">{yearId()}학년 필수 이수 과목 설정</div>
-        <div className="grid grid-cols-3 gap-4 my-4">
-          {groupSubjects
-            ? groupSubjects.map((data, key) => (
+        <div className="grid grid-cols-2 gap-2 my-4">
+          {filteredSubject
+            ? filteredSubject.map((data, key) => (
                 <button key={key} className="bg-slate-100 p-2 px-4 rounded-md">
                   <div>{data.title}</div>
                 </button>
