e0761401719105e982c74007096e968799b9aa44
jhyunwoo <jhyunwoo0228@gmail.com>
2023-02-05 23:16:11 +0900

    fix error

--- pages/admin/[schoolId]/index.jsx
+++ pages/admin/[schoolId]/index.jsx
@@ -17,21 +17,15 @@ export default function SchoolAdminPage() {
       <div className="grid grid-cols-2 gap-4 p-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8">
         <SchoolName />
         <DataCard link={`/admin/${school ? school.code : null}/students`} title={"학생"}>
-          <p className={!students ? "bg-slate-300 rounded-sm animate-pulse flex justify-center items-center" : ""}>
-            {students ? students.length : "..."}
-          </p>
+          <p>{students ? students.length : "..."}</p>
         </DataCard>
 
         <DataCard link={`/admin/${school ? school.code : null}/school/subjects`} title={"교과목"}>
-          <p className={!subjects ? "bg-slate-300 rounded-sm p-1 animate-pulse flex justify-center items-center" : ""}>
-            {subjects ? subjects.length : "..."}
-          </p>
+          <p>{subjects ? subjects.length : "..."}</p>
         </DataCard>
 
         <DataCard link={`/admin/${school ? school.code : null}/school/diploma`} title={"디플로마"}>
-          <p className={!diplomas ? "bg-slate-300 rounded-full animate-pulse flex justify-center items-center" : ""}>
-            {diplomas ? diplomas.length : "..."}
-          </p>
+          <p>{diplomas ? diplomas.length : "..."}</p>
         </DataCard>
       </div>
     </ProtectedPage>
